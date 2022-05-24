9c8509724594385454d851f87275baec3de6b857
Drew <drewbdoo@gmail.com>
2022-05-24 10:47:58 -0400

    uploading files

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,63 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        int N = 9;
+        int arr[] = { 2, 1, 6, 10, 4, 1, 3, 9, 7};
+        quickSort(arr,0, 8 );
+        System.out.println(Arrays.toString(arr));
+
+
+
+
+    }
+
+    static void bubblesort(int[] arr, int n) {
+        int i, j;
+        for(i=0; i<n-1; i++){
+            for(j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]= temp;
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+
+    public static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+
+
+
+
+}
